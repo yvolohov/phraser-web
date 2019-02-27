22ce4cb56681b71ec16c680c4cbccfc534134782
Yaroslav Volohov <yvolohov@gmail.com>
2019-02-27 18:30:32 +0200

    added grid

---
+++ resources/views/trainer.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Trainer</title>
+        <link href="{{ asset('css/grid.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container">
+
+            <div class="row">
+                <div class="col-2-sm">
+                </div>
+                <div class="col-8-sm" style="text-align: center;">
+                    <b>Hello !!!</b>
+                </div>
+                <div class="col-2-sm">
+                </div>
+            </div>
+
+        </div>
+    </body>
+</html>
