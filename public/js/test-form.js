fef68c56d4f63b87a04742f42e63c31dab9df4e6
Yaroslav Volohov <yvolohov@gmail.com>
2019-02-28 12:18:55 +0200

    improved test form

---
+++ public/js/test-form.js
@@ -0,0 +1,181 @@
+$(document).ready(function() {
+    /* Разбивает предложение на токены */
+    function Tokenizer() {
+        this.pattern = new RegExp("^[A-Za-zА-Яа-я0-9\'\"\-]$");
+
+        this.getSymbolType = function(symbol) {
+            var charCode = symbol.charCodeAt(0);
+
+            if (charCode === 9 || charCode === 32) {
+                return 0;
+            }
+            else if (this.pattern.exec(symbol) != null) {
+                return 1;
+            }
+            else {
+                return 2;
+            }
+        };
+
+        this.tokenizeSentence = function(sentence) {
+            tokSentence = "";
+            sentence = sentence.trim();
+            isDivider = false;
+
+            if (sentence.length > 0) {
+                tokSentence = sentence.substr(0, 1).toUpperCase();
+            }
+
+            for (var index = 1; index < sentence.length; index++) {
+                var previousSymbol = sentence.substr(index - 1, 1);
+                var currentSymbol = sentence.substr(index, 1);
+                var previousSymbolType = this.getSymbolType(previousSymbol);
+                var currentSymbolType = this.getSymbolType(currentSymbol);
+
+                if (previousSymbolType !== currentSymbolType && !(isDivider)) {
+                    tokSentence += "|";
+                    isDivider = true;
+                }
+
+                if (currentSymbolType > 0) {
+                    tokSentence += currentSymbol.toUpperCase();
+                    isDivider = false;
+                }
+            }
+            return tokSentence;
+        };
+    }
+
+    /* Управляет процессом тестирования */
+    function TestManager(tokenizer) {
+        this.progressField = $("#text-progress");
+        this.questionField = $("#text-question");
+        this.answerField = $("#input-answer");
+        this.tokenizer = tokenizer;
+        this.questions = [];
+        this.questionIndex = -1;
+
+        this.start = function(data) {
+            $(".start-screen").hide();
+            $(".main-screen").show();
+
+            if (data.length > 0) {
+                this.answerField.removeAttr("disabled").focus();
+                $("#button-answer").removeAttr("disabled");
+                $("#button-help").removeAttr("disabled");
+                this.questions = data;
+                this.next();
+            }
+            else {
+                this.progressField.text("");
+                this.questionField.text("Error: Cannot load questions from DB !!!");
+            }
+        };
+
+        this.next = function() {
+            this.questionIndex++;
+
+            if (this.questionIndex < this.questions.length) {
+                this.progressField.text("Q. " + (this.questionIndex + 1).toString() + " : " + (this.questions.length).toString());
+                this.questionField.text(this.questions[this.questionIndex].translation);
+                this.answerField.val("");
+            }
+            else {
+                this.endTest();
+            }
+        };
+
+        this.end = function() {
+            this.questionField.text("Test is passed !!!");
+            this.answerField.attr("disabled", "disabled");
+            this.answerField.val("");
+            $("#button-answer").attr("disabled", "disabled");
+            $("#button-help").attr("disabled", "disabled");
+        };
+
+        this.refreshAnswerColor = function(tokSentence, dbSentence) {
+            if (tokSentence.length < dbSentence.length && dbSentence.indexOf(tokSentence) === 0) {
+                this.answerField.css("color", "#436EEE");
+            }
+            else if (tokSentence.length === dbSentence.length && dbSentence.indexOf(tokSentence) === 0) {
+                this.answerField.css("color", "#008000");
+            }
+            else {
+                this.answerField.css("color", "#FF0000");
+            }
+        };
+
+        this.onAnswer = function() {
+            var tokSentence = this.tokenizer.tokenizeSentence(this.answerField.val());
+            var sentence = this.questions[this.questionIndex].phrase;
+            var dbSentence = this.tokenizer.tokenizeSentence(sentence);
+
+            if (tokSentence === dbSentence) {
+                this.next();
+            }
+            this.answerField.focus();
+        };
+
+        this.onHelp = function() {
+            this.answerField.val(this.questions[this.questionIndex].phrase).focus();
+            this.answerField.css("color", "#008000");
+        };
+
+        this.onAnswerFieldChange = function(e) {
+            var tokSentence = this.tokenizer.tokenizeSentence(this.answerField.val());
+            var sentence = this.questions[this.questionIndex].phrase;
+            var dbSentence = this.tokenizer.tokenizeSentence(sentence);
+
+            if (e.keyCode === 13 && tokSentence === dbSentence) {
+                this.next();
+            }
+            else {
+                this.refreshAnswerColor(tokSentence, dbSentence);
+            }
+        };
+
+        this.startTest = function() {
+            var self = this;
+
+            $.ajax({
+                type: "GET",
+                url: "/api/phrases",
+                data: {},
+                success: function(data) {
+                    self.start(data);
+                },
+                error: function(data) {
+                    console.log(data);
+                }
+            });
+        };
+
+        this.endTest = function() {
+            var self = this;
+            self.end();
+        };
+    }
+
+    var tokenizer = new Tokenizer();
+    var testManager = new TestManager(tokenizer);
+
+    /* Кнопка старта */
+    $("#button-start").click(function() {
+        testManager.startTest();
+    });
+
+    /* Кнопка ответа */
+    $("#button-answer").click(function() {
+        testManager.onAnswer();
+    });
+
+    /* Кнопка помощи */
+    $("#button-help").click(function() {
+        testManager.onHelp();
+    });
+
+    /* При изменении поля ввода ответа */
+    $("#input-answer").keyup(function(e) {
+        testManager.onAnswerFieldChange(e);
+    });
+});
--- resources/views/trainer.blade.php
+++ resources/views/trainer.blade.php
@@ -3,21 +3,45 @@
     <head>
         <meta charset="utf-8">
         <title>Trainer</title>
-        <link href="{{ asset('css/grid.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/restyle.min.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/test-form.css') }}" rel="stylesheet">
+        <script type="text/javascript" src="{{ asset('js/jquery-2.2.3.min.js') }}"></script>
+        <script type="text/javascript" src="{{ asset('js/test-form.js') }}"></script>
     </head>
     <body>
         <div class="container">
-
-            <div class="row">
-                <div class="col-2-sm">
+            <div class="grid condensed">
+                <div class="row start-screen">
+                    <div class="cell">
+                        <button class="button rounded primary" id="button-start">Начать упражнение</button>
+                    </div>
                 </div>
-                <div class="col-8-sm" style="text-align: center;">
-                    <b>Hello !!!</b>
+                <div class="row main-screen" style="display: none;">
+                    <div class="cell">
+                        <h5 id="text-progress">Загрузка ...</h5>
+                    </div>
                 </div>
-                <div class="col-2-sm">
+                <div class="row main-screen" style="display: none;">
+                    <div class="cell">
+                        <h3 id="text-question">Загрузка ...</h3>
+                    </div>
+                </div>
+                <div class="row main-screen" style="display: none;">
+                    <div class="cell">
+                        <div class="input-control input-text full-size">
+                            <input id="input-answer" type="text" disabled="disabled">
+                        </div>
+                    </div>
+                </div>
+                <div class="row main-screen" style="display: none;">
+                    <div class="cell">
+                        <div id="button-container">
+                            <button class="button rounded primary" id="button-answer" type="button" disabled="disabled">Ответить</button>
+                            <button class="button rounded" id="button-help" type="button" disabled="disabled">Подсказка</button>
+                        </div>
+                    </div>
                 </div>
             </div>
-
         </div>
     </body>
 </html>
