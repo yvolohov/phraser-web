800a8ba0360cc973b8f0204ec9fedc9a702a2659
Yaroslav Volohov <yvolohov@gmail.com>
2019-03-26 11:55:34 +0200

    added real passages

--- app/Http/Controllers/PhraseController.php
+++ app/Http/Controllers/PhraseController.php
@@ -15,6 +15,7 @@ class PhraseController extends Controller
             phrases.phrase,
             phrases.translation,
             IFNULL(tests.passages_cnt, 0) AS passages_cnt,
+            IFNULL(tests.real_passages_cnt, 0) AS real_passages_cnt,
             IFNULL(tests.first_passage, '0000-00-00 00:00:00') AS first_passage,
             IFNULL(tests.last_passage, '0000-00-00 00:00:00') AS last_passage,
             IF(tests.phrase_id IS NULL, 0, 1) AS test_exists
@@ -35,6 +36,7 @@ class PhraseController extends Controller
             "SELECT
             phrases.id,
             IFNULL(tests.passages_cnt, 0) AS passages_cnt,
+            IFNULL(tests.real_passages_cnt, 0) AS real_passages_cnt,
             IF(tests.phrase_id IS NULL, 0, 1) AS test_exists
             FROM phrases
             LEFT JOIN tests
@@ -49,10 +51,12 @@ class PhraseController extends Controller
 
         $testExists = $results[0]->test_exists;
         $passagesCnt = $results[0]->passages_cnt;
+        $realPassagesCnt = $results[0]->real_passages_cnt;
         $newPassagesCnt = ($passagesCnt < 3) ? $passagesCnt + 1 : $passagesCnt;
+        $newRealPassagesCnt = $realPassagesCnt + 1;
 
         if ($testExists) {
-            $this->updateTest($phraseId, $newPassagesCnt);
+            $this->updateTest($phraseId, $newPassagesCnt, $newRealPassagesCnt);
         }
         else {
             $this->insertTest($phraseId);
@@ -64,19 +68,22 @@ class PhraseController extends Controller
     private function insertTest($phraseId)
     {
         DB::insert(
-            "INSERT INTO tests (phrase_id, passages_cnt, first_passage, last_passage)
-            VALUES (:phrase_id, 1, NOW(), NOW())",
+            "INSERT INTO tests (phrase_id, passages_cnt, real_passages_cnt, first_passage, last_passage)
+            VALUES (:phrase_id, 1, 1, NOW(), NOW())",
             ['phrase_id' => $phraseId]
         );
     }
 
-    private function updateTest($phraseId, $passagesCnt)
+    private function updateTest($phraseId, $passagesCnt, $realPassagesCnt)
     {
         DB::update(
             "UPDATE tests 
-            SET passages_cnt = :passages_cnt, last_passage = NOW()
-            WHERE phrase_id = :phrase_id",
-            ['phrase_id' => $phraseId, 'passages_cnt' => $passagesCnt]
+            SET passages_cnt = :passages_cnt, real_passages_cnt = :real_passages_cnt, last_passage = NOW()
+            WHERE phrase_id = :phrase_id", [
+                'phrase_id' => $phraseId,
+                'passages_cnt' => $passagesCnt,
+                'real_passages_cnt' => $realPassagesCnt
+            ]
         );
     }
 }
